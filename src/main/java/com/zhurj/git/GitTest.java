02de29018e01377fe95995d116bbdcb8ce1ce376
zhurj <1315070695@qq.com>
2023-02-19 11:59:13 +0800

    test

---
+++ src/main/java/com/zhurj/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zhurj.git;
+
+/**
+ * @program: git-project
+ * @description:
+ * @author: zhurj
+ * @create: 2023-02-19 11:39
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
